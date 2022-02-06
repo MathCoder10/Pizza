68fd6766acbcda3bdd5e87abc6ac882240899eb8
MathCoder10 <81539939+MathCoder10@users.noreply.github.com>
2022-02-06 09:38:21 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function show() {
+    document.getElementById("lpecd").innerHTML = "the pizza is so delicious!";
+}
+
+function hide() {
+    document.getElementById("lpecd").innerHTML = "la pizza è così deliziosa!";
+}
